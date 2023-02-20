a41f24cc1117e77f3f4e9f0f989330125c51726a
emereum <24802844+emereum@users.noreply.github.com>
2023-02-20 20:52:58 +1100

    Niceties

--- github-cromulent-prs.user.js
+++ github-cromulent-prs.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/emereum/tampermonkey-scripts
 // @updateURL    https://raw.githubusercontent.com/emereum/tampermonkey-scripts/main/github-cromulent-prs.user.js
 // @downloadURL  https://raw.githubusercontent.com/emereum/tampermonkey-scripts/main/github-cromulent-prs.user.js
-// @version      0.3
+// @version      0.4
 // @description  Reverses the discussion order in GitHub PRs.
 // @author       emereum
 // @match        https://github.com/*/*/pull/*
@@ -14,17 +14,21 @@
 (function() {
     'use strict';
 
-    const styles = Object.assign(document.createElement('style'), {
-        innerHTML: `
-            @media (min-width: 768px) {
-            .bootleg-comment-box {
-                margin-left: -56px;
-            }
-            }
-        `});
-    document.head.appendChild(styles);
-
     const reverseChildren = (el) => el.append(...Array.from(el.childNodes).reverse());
+    const addStyle = (styles) => {
+        const el = Object.assign(document.createElement('style'), {
+            innerHTML: styles
+        });
+        document.head.appendChild(el);
+    }
+
+    addStyle(`
+      @media (min-width: 768px) {
+        .bootleg-comment-box {
+          margin-left: -56px;
+        }
+      }
+    `);
 
     // Reverse the discussion
     const discussion = document.querySelector('.js-discussion');
@@ -32,6 +36,7 @@
         .reverse()
         .forEach(x => {
             x.remove();
+            x.classList.add('can-reverse-header');
             discussion.appendChild(x);
         });
 
@@ -59,4 +64,32 @@
 
     // Remove 'Attach files' hint
     cb.querySelector('.hx_drag-and-drop').remove();
+
+    // Move the title bar of each comment to the bottom of the comment
+    Array.from(document.querySelectorAll('.timeline-comment'))
+        .forEach(comment => {
+        const header = comment.firstElementChild;
+        header.remove();
+        comment.appendChild(header);
+    });
+
+    // Only re-style the comment header box if we haven't blown up yet
+    addStyle(`
+      .can-reverse-header .timeline-comment-header {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+      .can-reverse-header .TimelineItem-avatar {
+        bottom: 16px;
+      }
+      .can-reverse-header .timeline-comment--caret::after,
+      .can-reverse-header .timeline-comment--caret::before,
+      .can-reverse-header .timeline-comment--caret.current-user::after,
+      .can-reverse-header .timeline-comment--caret.current-user::before {
+        top: inherit;
+        bottom: 11px;
+      }
+    `);
 })();
